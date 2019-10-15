a69cc619fb51c64e01ac6ee37cda9192ef624524
caoyuqing-hash <321139217@qq.com>
2019-10-15 20:03:18 +0800

    fifth commit

---
+++ a/js/index.js
@@ -0,0 +1,8 @@
+$(function(){
+    $('.nav-device-item').eq(1).click(function(){
+        alert("敬请期待~");
+    });
+    $('.nav-device-item').eq(2).click(function(){
+        alert("敬请期待~");
+    });
+});
